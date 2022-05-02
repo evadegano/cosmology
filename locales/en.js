47aacb1ca33fc23e08bf789c3552eff58f24914d
evanina <eva.degano@gmail.com>
2022-05-02 11:34:51 +0200

    initial commit

---
+++ locales/en.js
@@ -0,0 +1,54 @@
+const home = {
+  navLinkOne: "Login",
+  navLinkTwo: "About us",
+  header: "Your curated spiritual marketplace and moodboard.",
+  formTitle: "What is your goal focus?",
+  cta: "Next",
+  sectionOneTitle: "What's meant for you will simply find you",
+  sectionOneText: "Curated spiritual resources, content, inspo, connections, and shopping all in one place relevant to your goals, birthchart, and preferences ."
+}
+
+const preFooter = {
+  navLinkOne: "Get notifications",
+  navLinkTwo: "Recommend a product",
+  navLinkThree: "Host a live event",
+  socials: "Socials",
+  newsletter: "Bring the good vibes to your inbox",
+}
+
+const footer = {
+  navLinkOne: "Privacy center",
+  navLinkTwo: "Privacy and cookie policy",
+  navLinkThree: "Manage cookies",
+  navLinkFour: "Terms & conditions",
+  navLinkFive: "Copyright notice",
+}
+
+const userNavBar = {
+  navLinkOne: "Logout",
+}
+
+const profile = {
+  headerCTAOne: "Sell a product",
+  headerCTATwo: "See birth chart karma",
+  headerCTAThree: "Host a live event",
+  sectionOneTitle: "Your messages",
+  sectionOneCTAOne: "say it to the universe",
+  sectionOneCTATwo: "talk to me nice",
+  sectionTwoCTAOne: "Feed",
+  sectionTwoCTATwo: "Community",
+  sectionTwoCTAThree: "New",
+  sectionTwoCTAFour: "Saved",
+}
+
+const forms = {
+  searchPlaceholder: "search",
+  newsletterButton: "subscribe"
+}
+
+const itemTypes = ["all", "podcasts", "meditations", "affirmations", "foods", "therapies"]
+
+const goals = ["weight loss", "clarity", "earning more money", "positivity", "finding love", "relationship improvement", "physical healing", "reducing PMS", "focus", "productivity", "mental toughness"]
+
+
+export { home, preFooter, footer, userNavBar, profile, forms, itemTypes } 
